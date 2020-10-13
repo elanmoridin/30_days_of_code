4ec2939addda7bf5c02798ca206ba873802b0acc
Zach Doll <zach.doll@gmail.com>
2020-10-13 17:53:22 -0500

    did day 2 and 3

---
+++ day02.js
@@ -0,0 +1,40 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+
+
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+    let tip = meal_cost * (tip_percent / 100);
+    const tax_percent = parseInt(readLine(), 10);
+    let tax = meal_cost * (tax_percent / 100);
+    let total_cost = meal_cost + tip + tax;
+    console.log(Math.round(total_cost));
+}
